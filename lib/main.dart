400eb3046c0ce9e97438f1a8d0194185b8b044df
pathYu <1974675011@qq.com>
2022-02-04 23:47:32 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('flutter test'),
+            centerTitle: true,
+          ),
+          body: const Center(
+            child: Text('tessst'),
+          ),
+        ));
+  }
+}
