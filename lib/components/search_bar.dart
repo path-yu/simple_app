d9e2d4e71bb94e7dd0cbd7f7d7daff513f743c1a
path-yu <1974675011@qq.com>
2022-02-05 21:23:35 +0800

    封装输入框组件

---
+++ lib/components/search_bar.dart
@@ -0,0 +1,60 @@
+import "package:flutter/material.dart";
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SearchBar extends StatefulWidget {
+  final TextEditingController _todoController;
+  final void Function(String) addConfrim;
+  final TextInputAction textInputAction;
+  final String placeHolder;
+  Color fillColor;
+  Icon prefixIcon;
+  int inputWidth;
+  SearchBar(this._todoController, this.addConfrim, this.textInputAction,
+      this.placeHolder,
+      {Key? key,
+      required this.prefixIcon,
+      this.fillColor = Colors.white,
+      this.inputWidth = 250})
+      : super(key: key);
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+          maxHeight: ScreenUtil().setSp(30),
+          maxWidth: ScreenUtil().setSp(widget.inputWidth)),
+      child: TextField(
+        controller: widget._todoController,
+        textInputAction: widget.textInputAction,
+        onSubmitted: widget.addConfrim,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 4.0),
+          hintText: widget.placeHolder,
+          hintStyle: TextStyle(fontSize: ScreenUtil().setSp(15)),
+          prefixIcon: widget.prefixIcon,
+          // 设置右边图标
+          suffixIcon: IconButton(
+              icon: const Icon(Icons.clear),
+              iconSize: ScreenUtil().setSp(15),
+              highlightColor: Colors.white,
+              onPressed: () => widget._todoController.text = ""),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none),
+          filled: true,
+          fillColor: widget.fillColor,
+        ),
+      ),
+    );
+  }
+}
--- lib/page/todo_list_page.dart
+++ lib/page/todo_list_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:simple_app/components/base/buildBaseAppBar.dart';
+import 'package:simple_app/components/search_bar.dart';
+import 'package:simple_app/generated/l10n.dart';
+
+import '../utils/showToast.dart';
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
@@ -8,9 +13,37 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  //定义一个controller
+  final TextEditingController _todoController = TextEditingController();
+
+  // 监听键盘点击了确认按钮
+  void addConfrim(String value) {
+    if (value.isEmpty) {
+      showToast('不能为空');
+    } else {
+      // 向通知栏添加一个消息
+      // showNotification('您添加了一条新todo, 请尽快完成哦');
+      // addTodoLitItem();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // ignore: prefer_const_constructors
-    return Text('333');
+    return Scaffold(
+      appBar: buildBaseAppBar(S.of(context).toolKit),
+      body: Center(
+        child: Column(
+          children: [
+            SearchBar(
+              _todoController,
+              addConfrim,
+              TextInputAction.go,
+              '添加todo',
+              prefixIcon: Icon(Icons.add),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
