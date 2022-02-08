961e73bdd4d87e4ffd2e915cc9f260b951e0b295
path-yu <1974675011@qq.com>
2022-02-08 19:29:47 +0800

    更新toast样式

--- lib/utils/show_toast.dart
+++ lib/utils/show_toast.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -6,5 +7,6 @@ showToast(String message) {
       msg: message,
       gravity: ToastGravity.CENTER,
       timeInSecForIosWeb: 1,
+      backgroundColor: const Color.fromRGBO(0, 0, 0, 0.6),
       fontSize: ScreenUtil().setSp(15));
 }
