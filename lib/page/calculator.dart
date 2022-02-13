da70f192c8ce3531b6da9a61397f1c0c9d63bf84
path-yu <1974675011@qq.com>
2022-02-13 20:41:14 +0800

    完成计算器基本布局

--- lib/page/calculator.dart
+++ lib/page/calculator.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:simple_app/components/base/build_base_app_bar.dart';
 import 'package:simple_app/generated/l10n.dart';
+import 'package:simple_app/main.dart';
+import 'package:simple_app/provider/current_theme.dart';
 
 class CalculatorPage extends StatefulWidget {
   const CalculatorPage({Key? key}) : super(key: key);
@@ -15,12 +19,96 @@ class _CalculatorPageState extends State<CalculatorPage> {
     super.initState();
   }
 
+  final Color textColor =
+      navigatorKey.currentState!.context.watch<CurrentTheme>().isNightMode
+          ? Colors.white
+          : const Color(0xff999999);
+  final primary = navigatorKey.currentState!.context
+      .watch<CurrentTheme>()
+      .themeBackgroundColor;
+  List operatorList = [
+    'AC',
+    '±',
+    '％',
+    '÷',
+    '7',
+    '8',
+    '9',
+    '×',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '0',
+    '.',
+    '='
+  ];
+
+  void handleOperator() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: buildBaseAppBar(S.of(context).calculator),
         body: Container(
-          child: Text('34'),
+          padding: EdgeInsets.all(ScreenUtil().setHeight(20)),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  // answer
+                  Text(
+                    '6',
+                    style: TextStyle(fontSize: ScreenUtil().setSp(48)),
+                    textAlign: TextAlign.right,
+                  ),
+                  // 运算表达式
+                  Text(
+                    '1x6',
+                    style: TextStyle(
+                        color: const Color(0xff666666),
+                        fontSize: ScreenUtil().setSp(30)),
+                    textAlign: TextAlign.right,
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Wrap(
+                    spacing: 15,
+                    runSpacing: 15,
+                    alignment: WrapAlignment.spaceBetween,
+                    children: operatorList.map((e) {
+                      if (e == '0') {}
+                      return ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: e == '0'
+                                  ? const Size(160, 75)
+                                  : const Size(75, 75),
+                              shape: e == '0'
+                                  ? const RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(35)),
+                                    )
+                                  : const CircleBorder(),
+                              primary: primary),
+                          child: Text(e,
+                              style: TextStyle(
+                                  color: textColor,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: ScreenUtil().setSp(24))));
+                    }).toList()),
+              )
+            ],
+          ),
         ));
   }
 }
--- lib/provider/current_theme.dart
+++ lib/provider/current_theme.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:simple_app/common/color.dart';
 import 'package:simple_app/common/global.dart';
-
-
 
 class CurrentTheme with ChangeNotifier {
   // 当前语言环境
@@ -12,6 +11,10 @@ class CurrentTheme with ChangeNotifier {
   CurrentTheme({this.themeMode = Brightness.light});
   // 是否为夜间模式
   bool get isNightMode => themeMode == Brightness.dark ? true : false;
+
+  // 暗色或白色背景色
+  Color get themeBackgroundColor =>
+      isNightMode ? easyDarkColor : const Color.fromRGBO(246, 246, 246, 1.0);
 
   changeMode(Brightness mode) async {
     themeMode = mode;
